cdee4304fcc449f276924ff7fc7bb64a977498cd
Roland Firmont <qowync@gmail.com>
2016-08-06 14:18:43 +0200

    new function for writing json using JsonGenerator and a Consumer

--- src/main/java/qowyn/ark/tools/CommonFunctions.java
+++ src/main/java/qowyn/ark/tools/CommonFunctions.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonWriterFactory;
 import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonGeneratorFactory;
 
 import qowyn.ark.ArkSavegame;
 import qowyn.ark.GameObject;
@@ -40,6 +42,18 @@ public class CommonFunctions {
 
       JsonWriterFactory jwf = Json.createWriterFactory(properties);
       jwf.createWriter(out).writeObject(o);
+    }
+  }
+
+  public static void writeJson(String outFile, Consumer<JsonGenerator> writeJson) throws IOException {
+    try (PrintWriter out = new PrintWriter(outFile)) {
+      Map<String, Object> properties = new HashMap<>(1);
+      properties.put(JsonGenerator.PRETTY_PRINTING, true);
+
+      JsonGeneratorFactory jgf = Json.createGeneratorFactory(properties);
+      JsonGenerator jg = jgf.createGenerator(out);
+      writeJson.accept(jg);
+      jg.close();
     }
   }
 
